509fd112c2b980e858af08897e87307dfc2f86d2
Nobuo Hayashi <haya.biz@gmail.com>
2017-04-01 01:18:53 +0900

    debug

--- sendAllAtScheduledTime.py
+++ sendAllAtScheduledTime.py
@@ -420,7 +420,8 @@ if __name__ == '__main__':
 
             fp = open(dir_path + '/'+ 'mochimugi.log', 'r')
             print 'Opened log file'
-            for num_lines in range(total_lines - 20, total_lines - 1):
+            for num_lines in range(1570, 1590):
+            # for num_lines in range(total_lines - 20, total_lines - 1):
                 last20linesLog = fp.readlines()[num_lines]
             # last20linesLog = fp.readlines()[total_lines-1]
             fp.close
