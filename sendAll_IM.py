11a9c1dbd482c675fcb24a795478d9925104ffce
Nobuo Hayashi <haya.biz@gmail.com>
2017-06-05 16:50:02 +0900

    stop logging from rc.local, logging function is provided by python program itself

--- sendAll_IM.py
+++ sendAll_IM.py
@@ -62,7 +62,7 @@ global_ipAddress =  commands.getoutput('hostname -I')
 print "Global IP Address is : %s" % global_ipAddress
 
 logging.basicConfig(filename=dir_path + '/'+ 'mochimugi.log', level=logging.DEBUG, format='%(asctime)s %(message)s')
-logging.warning('Global IP Address:%s', global_ipAddress)
+logging.warning('logging.warning:Global IP Address:%s', global_ipAddress)
 
 def sendLog_ftps(file_name):
     try:
