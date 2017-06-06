bcc320b0e92461d135780095fb1c1490a164f13b
Nobuo Hayashi <haya.biz@gmail.com>
2017-06-07 02:45:23 +0900

    type convert:  logger.debug(str(response.status), str(response.reason))

--- sendAll_IM.py
+++ sendAll_IM.py
@@ -469,7 +469,7 @@ if __name__ == '__main__':
             #deployに"sandBox"と書いてあれば、putDataAPI_withAuth.phpが自動判別してsandBoxサーバーにデータを送る
             logger.debug("connection requested")
             response = conn.getresponse()
-            logger.debug(response.status, response.reason)
+            logger.debug(str(response.status), str(response.reason))
             data = response.read()
             logger.debug(data)
             conn.close()
