2a8335f1cdc13bd338fd16b4f7d29781223656a1
Nobuo Hayashi <haya.biz@gmail.com>
2017-06-10 01:40:34 +0900

    redirect as root user

--- rcLocalUpdate.py
+++ rcLocalUpdate.py
@@ -6,7 +6,7 @@ import os
 def updateRCLocal():
 #    os.system("/etc/rc.local < rcLocalUpdate.sh")
 
-    os.system("/home/pi/Documents/mochimugi/sendAllDataPicture/rcLocalUpdate.sh > /etc/rc.local")
+    os.system("sudo /home/pi/Documents/mochimugi/sendAllDataPicture/rcLocalUpdate.sh > /etc/rc.local")
 
 updateRCLocal()
 
