c15a39c79aa4c074de73b4b3db968f229751ab91
Nobuo Hayashi <haya.biz@gmail.com>
2017-06-12 00:34:47 +0900

    test return 0

--- test/waitForPing_2.sh
+++ test/waitForPing_2.sh
@@ -6,7 +6,7 @@ for i in {1..3};
     do ping -c1 192.168.12.8 &> /dev/null && (echo Server is available) && break; 
     echo -n .
     done
-[ $i = 3 ] && echo Can not reach Server && echo this is inline && return 1
+[ $i = 3 ] && echo Can not reach Server && return 0
 echo just test 
 return 0
 }
