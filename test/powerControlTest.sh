fea758e0bb95a42ceeb1874310b7730ba6f0834a
Nobuo Hayashi <haya.biz@gmail.com>
2017-06-11 13:21:30 +0900

    create new log file

--- test/powerControlTest.sh
+++ test/powerControlTest.sh
@@ -2,7 +2,7 @@
 
 function my_shutdown2() {
   powerControlCommand="sudo /usr/sbin/i2cset -y 1 0x40 15 0 i"
-  eval $powerControlCommand | tee -a ./test.log
+  eval $powerControlCommand | tee -a ./logTest.log
   sleep 10
   sudo poweroff
   echo system will poweroff after 10 seconds, and reboot
