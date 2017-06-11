ff510a948c8d896a61e2bdfdfa9c4695e73ef104
Nobuo Hayashi <haya.biz@gmail.com>
2017-06-11 17:53:52 +0900

    標準エラーを取るのに |& を使ってみた

--- test/powerControlTest.sh
+++ test/powerControlTest.sh
@@ -4,13 +4,13 @@ trap "echo Encounterd error; exit 1" ERR
 
 function my_shutdown2() {
     powerControlCommand="sudo /usr/sbin/i2cset -y 1 0x41 15 0 i"
-    if [eval $powerControlCommand 2>&1 | grep "Error"]; then
+    if [eval $powerControlCommand |& grep "Error"]; then
         echo "Error encountered"
         exit 1
         fi
     echo system will poweroff after 10 seconds, and reboot
-    sleep 10
-    sudo poweroff
+    # sleep 10
+    # sudo poweroff
     exit 0
 }
 my_shutdown2
