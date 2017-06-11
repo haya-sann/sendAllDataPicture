5f7f0a44d2d60998258f7b6e9a26fe353d6ba6ee
Nobuo Hayashi <haya.biz@gmail.com>
2017-06-11 23:20:00 +0900

    ping test two location

--- test/waitForPing_2.sh
+++ test/waitForPing_2.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
-function waitForPing() {
+function waitForPing1() {
     # Wait for Network to be available.
-for i in {1..5};
+for i in {1..3};
     do ping -c1 192.168.12.8 &> /dev/null && (Server is available) && break; 
     echo -n .
     done
-[ $i == 5 ] && ( echo Can not reach to Server ; exit 1)
+[ $i == 3 ] && ( echo Can not reach to Server ; exit 1)
 return 1
 }
 
-waitForPing || ( echo network error ; exit 1 )
+function waitForPing2() {
+    # Wait for Network to be available.
+for i in {1..3};
+    do ping -c1 127.0.0.1 &> /dev/null && (Server is available) && break; 
+    echo -n .
+    done
+[ $i == 3 ] && ( echo Can not reach to Server ; exit 1)
+return 1
+}
+
+waitForPing1 || ( echo network error ; exit 1 )
+waitForPing2 || ( echo network error ; exit 1 )
 echo network available
