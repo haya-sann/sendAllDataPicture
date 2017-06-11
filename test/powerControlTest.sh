bd5b99821ee67d28bac2ff95e1a6cea211551a8f
Nobuo Hayashi <haya.biz@gmail.com>
2017-06-11 23:07:44 +0900

    waitForPing now try 5 times

--- test/powerControlTest.sh
+++ test/powerControlTest.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-trap "echo Encounterd error; exit 1" ERR
-
 function my_shutdown2() {
     powerControlCommand="sudo /usr/sbin/i2cset -y 1 0x41 15 0 i"
     if eval $powerControlCommand |& grep "Error"; then
@@ -9,8 +7,8 @@ function my_shutdown2() {
         exit 1
         fi
     echo system will poweroff after 10 seconds, and reboot
-    # sleep 10
-    # sudo poweroff
+    sleep 10
+    sudo poweroff
     exit 0
 }
 my_shutdown2
--- test/waitForPing_2.sh
+++ test/waitForPing_2.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-for i in {1..10};
-do ping -c1 192.168.12.104 &> /dev/null && echo SakuraServer is available; break;
-echo -n .
-done
-echo Can not reach to Sakura Server.
+function waitForPing() {
+    # Wait for Network to be available.
+for i in {1..5};
+    do ping -c1 192.168.12.8 &> /dev/null && (Server is available) && break; 
+    echo -n .
+    done
+[ $i = 5 ] && ( echo Can not reach to Sakura Server ; exit 1)
+return 0
+}
+
+
+waitForPing || ( echo network error ; exit 1 )
+echo network available
