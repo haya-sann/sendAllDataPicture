f30388fc665b7ab4e9457a6f548f57996107f26d
Nobuo Hayashi <haya.biz@gmail.com>
2017-06-11 23:23:54 +0900

    ping to google.com

--- test/waitForPing_2.sh
+++ test/waitForPing_2.sh
@@ -13,7 +13,7 @@ return 1
 function waitForPing2() {
     # Wait for Network to be available.
 for i in {1..3};
-    do ping -c1 127.0.0.1 &> /dev/null && (Server is available) && break; 
+    do ping -c1 google.com &> /dev/null && (Server is available) && break; 
     echo -n .
     done
 [ $i == 3 ] && ( echo Can not reach to Server ; exit 1)
@@ -22,4 +22,5 @@ return 1
 
 waitForPing1 || ( echo network error ; exit 1 )
 waitForPing2 || ( echo network error ; exit 1 )
+
 echo network available
