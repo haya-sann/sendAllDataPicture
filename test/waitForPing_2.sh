cd71d0565c21cd9e68236c75d78bd28b972cd7a8
Nobuo Hayashi <haya.biz@gmail.com>
2017-06-11 23:12:28 +0900

    fix return value

--- test/waitForPing_2.sh
+++ test/waitForPing_2.sh
@@ -6,10 +6,9 @@ for i in {1..5};
     do ping -c1 192.168.12.8 &> /dev/null && (Server is available) && break; 
     echo -n .
     done
-[ $i = 5 ] && ( echo Can not reach to Sakura Server ; exit 1)
-return 0
+[ $i == 5 ] && ( echo Can not reach to Server ; exit 1)
+return 1
 }
-
 
 waitForPing || ( echo network error ; exit 1 )
 echo network available
