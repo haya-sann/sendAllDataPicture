102a2f254c10f70783c4ce53d881015981ae3901
Nobuo Hayashi <haya.biz@gmail.com>
2017-06-12 01:29:49 +0900

    write error なら何もしないで終わる

--- test/powerControlTest.sh
+++ test/powerControlTest.sh
@@ -2,13 +2,29 @@
 
 function my_shutdown2() {
     powerControlCommand="sudo /usr/sbin/i2cset -y 1 0x41 15 0 i"
-    if eval $powerControlCommand |& grep "Error"; then
+
+    for i in {1..5}
+    do
+        if eval $powerControlCommand |& grep "Error" then
         echo "Error encountered"
-        exit 1
+        sleep 1
+        else
+        break
         fi
+    done
+
+
+[ $i = 5 ] && echo error writing I2C && return 1
+  return 0
+}
+
+function goToPowerOff() {
+
     echo system will poweroff after 10 seconds, and reboot
     sleep 10
     sudo poweroff
     exit 0
 }
-my_shutdown2
+
+my_shutdown2 && goToPowerOff
+ 
