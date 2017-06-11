9652537834a8b6da069e4470a407ca6a2a65d060
Nobuo Hayashi <haya.biz@gmail.com>
2017-06-11 14:26:52 +0900

    trap ERR

--- test/powerControlTest.sh
+++ test/powerControlTest.sh
@@ -4,7 +4,7 @@ function my_shutdown2() {
     powerControlCommand="sudo /usr/sbin/i2cset -y 1 0x40 15 0 i"
     eval $powerControlCommand 2>&1 | tee -a ./logTest.log
 
-    trap "echo Encounterd error; exit 1" 2
+    trap "echo Encounterd error; exit 1" ERR
     echo system will poweroff after 10 seconds, and reboot
     sleep 10
     sudo poweroff
