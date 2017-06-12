b18395c61c0b8d03af283211191acca9f4286055
Nobuo Hayashi <haya.biz@gmail.com>
2017-06-12 16:42:27 +0900

    test i2c command to 0x40

--- test/i2cWritetest.py
+++ test/i2cWritetest.py
@@ -8,16 +8,16 @@ x=0
 
 file = "StandardError.log"
 
-powerMonagementModule_controlCommand = 'sudo /usr/sbin/i2cset -y 1 0x41 10 ' + str(x) + ' i'
+powerMonagementModule_controlCommand = 'sudo /usr/sbin/i2cset -y 1 0x40 10 ' + str(x) + ' i'
 
 print "sending Power Control Command : "+powerMonagementModule_controlCommand
 
 try:
     retcode = subprocess.call(powerMonagementModule_controlCommand, shell=True)
     if retcode < 0:
-        print >>subprocess.sys.stderr, "Child was terminated by signal", -retcode
+        print >>subprocess.sys.stderr, "I2C command was terminated by signal", -retcode
     else:
-        print >>subprocess.sys.stderr, "Child returned", retcode
+        print >>subprocess.sys.stderr, "I2C command success", retcode
 except OSError as e:
     print >>subprocess.sys.stderr, "Execution failed:", e
 
