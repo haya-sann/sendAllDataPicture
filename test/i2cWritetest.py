035efff4c39648fe8f2969477ff705dee947ac10
Nobuo Hayashi <haya.biz@gmail.com>
2017-06-12 18:37:53 +0900

    retry 3 times to send i2c command

--- test/i2cWritetest.py
+++ test/i2cWritetest.py
@@ -9,26 +9,21 @@ import time
 x=0
 file = "StandardError.log"
 
-powerMonagementModule_controlCommand = 'sudo /usr/sbin/i2cset -y 1 0x41 10 ' + str(x) + ' i'
+powerMonagementModule_controlCommand = 'sudo /usr/sbin/i2cset -y 1 0x40 10 ' + str(x) + ' i'
 
 print "sending Power Control Command : "+powerMonagementModule_controlCommand
 
-process = Popen(powerMonagementModule_controlCommand, shell=True, stdout=PIPE, stderr=PIPE)
-output, err = process.communicate()
+for i in range(1,5):
+    try:
+        process = Popen(powerMonagementModule_controlCommand, shell=True, stdout=PIPE, stderr=PIPE)
+        output, err = process.communicate()
 
-if "Error" in err:
-    print "Error encounterd"
-else:
-    print >>sys.stderr, "I2C command success"
-    call('sudo poweroff', shell=True)
+        if "Error" in err:
+            print "Error encounterd"
+        else:
+            print >>sys.stderr, "I2C command success"
+            call('sudo poweroff', shell=True)
+            break
+    except OSError as e:
+        call("sudo reboot")
 
-    
-print output, err
-
-#     if "Error" in subprocess.sys.stderr:
-#         print >>subprocess.sys.stderr, "I2C Write command failed"
-#     else:
-#         print >>subprocess.sys.stderr, "I2C command success", retcode
-#         subprocess.call('sudo poweroff', shell=True)
-# except OSError as e:
-#     print >>subprocess.sys.stderr, "Execution failed:", e
