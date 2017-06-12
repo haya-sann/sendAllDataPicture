0a414a7978c57516f24e8541551819bc9a5aaa54
Nobuo Hayashi <haya.biz@gmail.com>
2017-06-12 17:08:46 +0900

    i2c writeコマンドは書込み失敗しても戻り値は1になっている。したがって、実際にstderrの中身を解析するしかない。stderrに"Error"の文字があるかどうかを調べる

--- test/i2cWritetest.py
+++ test/i2cWritetest.py
@@ -14,12 +14,10 @@ print "sending Power Control Command : "+powerMonagementModule_controlCommand
 
 try:
     retcode = subprocess.call(powerMonagementModule_controlCommand, shell=True)
-    if retcode < 0:
-        print >>subprocess.sys.stderr, "I2C command was terminated by signal", -retcode
+    if "Error" in subprocess.sys.stderr:
+        print >>subprocess.sys.stderr, "I2C Write command failed"
     else:
         print >>subprocess.sys.stderr, "I2C command success", retcode
         subprocess.call('sudo poweroff', shell=True)
 except OSError as e:
     print >>subprocess.sys.stderr, "Execution failed:", e
-
-
