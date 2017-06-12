7a2517addfbe13faeeabaabdbf6db5152f8c3e6c
Nobuo Hayashi <haya.biz@gmail.com>
2017-06-12 18:04:33 +0900

    check if Error is in the err

--- test/i2cWritetest.py
+++ test/i2cWritetest.py
@@ -3,7 +3,7 @@
 import sys
 sys.path.append('/usr/local/lib/python3.4/dist-packages/')
 
-from  subprocess import Popen, PIPE
+from  subprocess import Popen, PIPE, call
 import time
 
 x=0
@@ -16,6 +16,13 @@ print "sending Power Control Command : "+powerMonagementModule_controlCommand
 process = Popen(powerMonagementModule_controlCommand, shell=True, stdout=PIPE, stderr=PIPE)
 output, err = process.communicate()
 
+if "Error" in err:
+    print "Error encounterd"
+else:
+    print >>sys.stderr, "I2C command success"
+    call('sudo poweroff', shell=True)
+
+    
 print output, err
 
 #     if "Error" in subprocess.sys.stderr:
