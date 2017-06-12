879e3da204be53f22e010a97c03fa88f5880707e
Nobuo Hayashi <haya.biz@gmail.com>
2017-06-12 17:43:06 +0900

    change to use pipe in subprocess

--- test/i2cWritetest.py
+++ test/i2cWritetest.py
@@ -1,23 +1,25 @@
 #!/usr/bin/python
 #coding: utf-8
 
-import subprocess
+from  subprocess import Popen, PIPE
 import time
 
 x=0
-
 file = "StandardError.log"
 
 powerMonagementModule_controlCommand = 'sudo /usr/sbin/i2cset -y 1 0x41 10 ' + str(x) + ' i'
 
 print "sending Power Control Command : "+powerMonagementModule_controlCommand
 
-try:
-    retcode = subprocess.call(powerMonagementModule_controlCommand, shell=True)
-    if "Error" in subprocess.sys.stderr:
-        print >>subprocess.sys.stderr, "I2C Write command failed"
-    else:
-        print >>subprocess.sys.stderr, "I2C command success", retcode
-        subprocess.call('sudo poweroff', shell=True)
-except OSError as e:
-    print >>subprocess.sys.stderr, "Execution failed:", e
+process = Popen(powerMonagementModule_controlCommand,  stdout=PIPE, stderr=PIPE)
+output, err = process.communicate()
+
+print output, err
+
+#     if "Error" in subprocess.sys.stderr:
+#         print >>subprocess.sys.stderr, "I2C Write command failed"
+#     else:
+#         print >>subprocess.sys.stderr, "I2C command success", retcode
+#         subprocess.call('sudo poweroff', shell=True)
+# except OSError as e:
+#     print >>subprocess.sys.stderr, "Execution failed:", e
