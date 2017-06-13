964bfa4d16c62ce03ea183345dfe43244610df58
Nobuo Hayashi <haya.biz@gmail.com>
2017-06-14 01:10:46 +0900

    fix power control command

--- sendAll_IM.py
+++ sendAll_IM.py
@@ -430,7 +430,7 @@ if __name__ == '__main__':
         logger.info("Deepsleep in " + str(x) + "minutes")
         x = x / 5
         timeToOff = 40
-        powerControlCommand = 'sudo /usr/sbin/i2cset -y 1 0x40 ' + timeToOff + ' ' + str(x) + ' i'
+        powerControlCommand = 'sudo /usr/sbin/i2cset -y 1 0x40 ' + str(timeToOff) + ' ' + str(x) + ' i'
         #40秒後に電源オフ、最後のパラメーター×5分後に起動
 
         #logger.info('電源モジュールに送信するコマンド用意：' + powerControlCommand + ':40秒後にシャットダウン、最後のパラメーター×5分後に起動')
