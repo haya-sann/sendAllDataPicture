eb919d3c3f9338351e009af7047006fd898baec1
Nobuo Hayashi <haya.biz@gmail.com>
2017-06-14 18:31:41 +0900

    forgot type cast

--- sendAll_IM.py
+++ sendAll_IM.py
@@ -156,7 +156,7 @@ def capture_send():
     time.sleep(2) #これも入れ忘れてた　2017/06/14　12:59
     picamera.brightness = 55 #標準の50よりほんの少し明るめに
     picamera.contrast = 53 #標準の50よりほんの少しコントラストを強めに
-    logger.info("brightness:"+str(picamera.brightness)+" ,contrast:"+(picamera.contrast))
+    logger.info("brightness:"+str(picamera.brightness)+" ,contrast:"+str(picamera.contrast))
     picamera.annotate_background = picamera.Color('black')
     picamera.annotate_text = now.strftime('%Y-%m-%d %H:%M:%S')
     picamera.rotation = 180
