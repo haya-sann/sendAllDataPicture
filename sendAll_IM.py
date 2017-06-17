45bfad8614b56107a30570a1c59ea424a08998fe
Nobuo Hayashi <haya.biz@gmail.com>
2017-06-17 22:35:27 +0900

    removed unneccessory debug log

--- sendAll_IM.py
+++ sendAll_IM.py
@@ -488,7 +488,6 @@ if __name__ == '__main__':
             logger.info('sending data to さくらレンタルサーバー via INTER-Mediator')
 
             params_IM = urllib.urlencode({'c': str(imKey), 'date': str(d), 'temp': temp, 'temperature': temperature, 'pressure': pressure, 'humid': humid, 'lux' : lightLevel, 'v0' : voltage_ch1, 'v1' : voltage_ch2, 'memo' : memo, 'deploy' : DEPLOY_SWITCH})
-            logger.debug(params_IM)
 
             conn = httplib.HTTPSConnection("mochimugi.sakura.ne.jp")
             conn.request("GET", "/IM/im_build/webAPI/putDataAPI_withAuth.php?" + params_IM)
