a7bec6f49920c48182bba7d90056fed6e90a51a1
Nobuo Hayashi <haya.biz@gmail.com>
2017-06-18 02:54:34 +0900

    this should be wakeupTime

--- sendAll_IM.py
+++ sendAll_IM.py
@@ -463,7 +463,7 @@ if __name__ == '__main__':
         wakeupTime = now + timeToWait #起動時刻算出
 
 
-        logger.info('電源モジュールに送信するコマンド用意：' + powerControlCommand + ':' + str(timeToOff) + '秒後に電源オフ、' + timeToWait.strftime('%Y%m%d%H%M') + 'に起動')
+        logger.info('電源モジュールに送信するコマンド用意：' + powerControlCommand + ':' + str(timeToOff) + '秒後に電源オフ、' + wakeupTime.strftime('%Y%m%d%H%M') + 'に起動')
 
         temperature, pressure, humid = readData()
         logger.info("Calculate CPU temperature of Raspberry Pi in Degrees C")
