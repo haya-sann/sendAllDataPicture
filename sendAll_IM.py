559748b14d630e99af112d597280b3ed6e75bf8e
Nobuo Hayashi <haya.biz@gmail.com>
2017-06-20 12:36:48 +0900

    debug annotation

--- sendAll_IM.py
+++ sendAll_IM.py
@@ -164,7 +164,8 @@ def capture_send():
 #標準の50よりほんの少しコントラストを強めに
     logger.info("brightness:"+str(picamera.brightness)+" ,contrast:"+str(picamera.contrast))
     # picamera.annotate_background = picamera.Color('black')
-    picamera.annotate_text = now.strftime('%Y-%m-%d %H:%M:%S') + " , Contrast : " + str(picamera.contrast)
+    picamera.annotate_text = now.strftime('%Y-%m-%d %H:%M:%S')
+    # + " , Contrast : " + str(picamera.contrast)
     picamera.rotation = 180
     picamera.capture(dir_path+'/'+captureFile_name)
     
