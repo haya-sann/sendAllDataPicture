160788bb41bc28f856e49340231d640b0672ce98
Nobuo Hayashi <haya.biz@gmail.com>
2017-06-23 01:29:49 +0900

    removed process.communicate

--- sendAll_IM.py
+++ sendAll_IM.py
@@ -112,13 +112,13 @@ def sendLog_ftps(file_name):
         _ftps.cwd('/home/mochimugi/www/seasonShots/' + put_directory) #アップロード先ディレクトリに移動
         logger.info('Success : Change directory to: /home/mochimugi/www/seasonShots/' + put_directory)
 
-        ftps_storeCommand = "_ftps.storbinary('STOR ' + logfile_name, _file)"
+        _ftps.storbinary('STOR ' + logfile_name, _file)
 
-        process = Popen(ftps_storeCommand, shell=True, stdout=PIPE, stderr=PIPE)
-        output, err = process.communicate()
+        # process = Popen(ftps_storeCommand, shell=True, stdout=PIPE, stderr=PIPE)
+        # output, err = process.communicate()
 
-        for line in _file:
-            print line
+        # for line in _file:
+        #     print line
         
         _file.close()
         _ftps.quit()
