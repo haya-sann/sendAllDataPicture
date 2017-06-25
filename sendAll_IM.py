83f7d43e736036005ed9700dfc38b931582524ce
Nobuo Hayashi <haya.biz@gmail.com>
2017-06-25 15:56:42 +0900

    50 times try. wait 2 seconds

--- sendAll_IM.py
+++ sendAll_IM.py
@@ -339,7 +339,7 @@ def get_calib_param():
             digH[i] = (-digH[i] ^ 0xFFFF) + 1
 
 def measureLight():
-    for lightSense in range(0, 10):
+    for lightSense in range(1, 10):
         try:
             #bus = smbus.SMBus(0) # Rev 1 Pi uses 0
             bus = smbus.SMBus(1)  # Rev 2 Pi uses 1
@@ -351,7 +351,7 @@ def measureLight():
             break
         except Exception as error_inMeasureLight:
             logger.debug("Error during sensing light data: " + str(error_inMeasureLight) + " / " + str(lightSense) + " times trial")
-            time.sleep(1)
+            time.sleep(2)
     else:
         logger.debug("Error during sensing light data after : " + str(lightSense) +" times trial")
     
