8d062d39ee853a3647fa3e6024bacf92927edf70
Nobuo Hayashi <haya.biz@gmail.com>
2017-06-30 15:47:22 +0900

    focus adjust

---
+++ test/focusAdjust.py
@@ -0,0 +1,15 @@
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import time
+import picamera
+from picamera import PiCamera
+
+
+with picamera.PiCamera() as camera:
+    camera.resolution = (1024, 768)
+    camera.start_preview()
+    # Camera warm-up time
+    # time.sleep(2)
+    # camera.capture('/home/pi/nas/201607311122.jpg')
