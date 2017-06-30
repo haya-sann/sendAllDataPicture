5fb88f1934a2c83c965d01c3c7e12673f37e7860
Nobuo Hayashi <haya.biz@gmail.com>
2017-06-30 15:57:09 +0900

    program will continue until hit ctrlC

--- test/focusAdjust.py
+++ test/focusAdjust.py
@@ -10,6 +10,6 @@ from picamera import PiCamera
 with picamera.PiCamera() as camera:
     camera.resolution = (1024, 768)
     camera.start_preview()
+    while True:
     #Camera warm-up time
-    time.sleep(2)
-    # camera.capture('/home/pi/nas/201607311122.jpg')
+        time.sleep(2)
