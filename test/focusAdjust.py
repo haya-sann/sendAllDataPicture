b085091ee620c59ca4a48b1f7a12765be4f48f28
Nobuo Hayashi <haya.biz@gmail.com>
2017-06-30 15:49:48 +0900

    warmup time

--- test/focusAdjust.py
+++ test/focusAdjust.py
@@ -10,6 +10,6 @@ from picamera import PiCamera
 with picamera.PiCamera() as camera:
     camera.resolution = (1024, 768)
     camera.start_preview()
-    # Camera warm-up time
-    # time.sleep(2)
+    #Camera warm-up time
+    time.sleep(2)
     # camera.capture('/home/pi/nas/201607311122.jpg')
