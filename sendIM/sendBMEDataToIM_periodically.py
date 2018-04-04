20d1d72a40b87aad32164d09ca1a56aae8840dcc
Nobuo Hayashi <haya.biz@gmail.com>
2018-04-05 01:19:28 +0900

    indent

--- sendIM/sendBMEDataToIM_periodically.py
+++ sendIM/sendBMEDataToIM_periodically.py
@@ -102,9 +102,6 @@ if __name__ == '__main__':
 		logger.info('IOErrorです。デバイスが認識できません')
     #		logger.exception('Error in read bme280: %s', err)
     finally:
-		print('処理を終了しました')
+        print('処理を終了しました')
         os.system('sudo poweroff')
         
-
-
-
