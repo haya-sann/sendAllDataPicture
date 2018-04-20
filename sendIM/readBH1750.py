55c4f82a29d61fcbd55cc51f814f353425664a28
Nobuo Hayashi <haya.biz@gmail.com>
2018-04-20 22:48:37 +0900

    male class file for BH1750

---
+++ sendIM/readBH1750.py
@@ -0,0 +1,115 @@
+#!/usr/bin/python
+#coding: utf-8
+
+import smbus
+
+from logging import getLogger
+logger = getLogger(__name__) 
+
+bus_number = 1
+#bus = SMBus(bus_number) #元はこうなっていた。
+bus = smbus.SMBus(bus_number)
+
+
+class BH1750():
+    """ Implement BH1750 communication. """
+    # Define some constants from the datasheet
+    POWER_DOWN = 0x00 # No active state
+    POWER_ON = 0x01 # Power on
+    RESET = 0x07 # Reset data register value
+
+    # Start measurement at 0.5lx resolution. Time typically 120ms
+    # Device is automatically set to Power Down after measurement.
+    ONE_TIME_HIGH_RES_MODE_2 = 0x21
+
+    def __init__(self, bus, addr=0x23):
+        self.bus = bus
+        self.addr = addr
+        self.power_down()
+        self.set_sensitivity()
+
+    def _set_mode(self, mode):
+        self.mode = mode
+        self.bus.write_byte(self.addr, self.mode)
+
+    def power_down(self):
+        self._set_mode(self.POWER_DOWN)
+
+    def power_on(self):
+        self._set_mode(self.POWER_ON)
+
+    def reset(self):
+        self.power_on() #It has to be powered on before resetting
+        self._set_mode(self.RESET)
+
+    def oneshot_high_res2(self):
+        self._set_mode(self.ONE_TIME_HIGH_RES_MODE_2)
+
+    def set_sensitivity(self, sensitivity=150):
+        """ Set the sensor sensitivity.
+            Valid values are 31 (lowest) to 254 (highest), default is 150.
+        """
+        self.mtreg = sensitivity
+        self.power_on()
+        self._set_mode(0x40 | (self.mtreg >> 5))
+        self._set_mode(0x60 | (self.mtreg & 0x1f))
+        self.power_down()
+
+    def get_result(self):
+        """ Return current measurement result in lx. """
+        data = self.bus.read_word_data(self.addr, self.mode)
+        count = data >> 8 | (data&0xff)<<8
+        mode2coeff = 2 if (self.mode & 0x03) == 0x01 else 1
+        ratio = 1/(1.2 * (self.mtreg/69.0) * mode2coeff)
+        return ratio*count
+
+#    def wait_for_result(self, additional=0):#what is this mean? Setting 0 to parameter.
+    def wait_for_result(self, additional):#what is this mean? Setting 0 to parameter.
+        basetime = 0.018 if (self.mode & 0x03) == 0x03 else 0.128
+        time.sleep(basetime * (self.mtreg/69.0) + additional)
+
+#    def do_measurement(self, mode, additional_delay=0):
+#パラメータを0にリセットしている。これではadditional_delay の指定が有効にならないのでは？
+    def do_measurement(self, mode, additional_delay):
+        """
+        Perform complete measurement using command
+        specified by parameter mode with additional
+        delay specified in parameter additional_delay.
+        Return output value in Lx.
+        """
+        self.reset()
+        self._set_mode(mode)
+        self.wait_for_result(additional=additional_delay)
+        return self.get_result()
+
+    def measure_high_res2(self, additional_delay=1):#was 0. But didn't work.
+        return self.do_measurement(self.ONE_TIME_HIGH_RES_MODE_2, additional_delay)
+
+
+def measureLight():
+    for lightSense in range(1, 20):
+        try:
+            logger.info("Sensing light level...")
+            #bus = smbus.SMBus(0) # Rev 1 Pi uses 0
+            bus = smbus.SMBus(1)  # Rev 2 Pi uses 1
+            sensor = BH1750(bus)
+            logger.info("Light Sensitivity: {:d}".format(sensor.mtreg))
+            lightLevel = sensor.measure_high_res2()
+            logger.info("Light Level: " + str(lightLevel))
+            return lightLevel
+            break
+        except Exception as error_inMeasureLight:
+            logger.debug("Error during sensing light data: " + str(error_inMeasureLight) + " / " + str(lightSense) + " times trial")
+            time.sleep(2)
+    else:
+        logger.debug("Error during sensing light data after : " + str(lightSense) +" times trial")
+
+
+if __name__ == '__main__':
+	try:
+        lightLevel =0 #init light level
+        lightLevel = measureLight()
+        print(lightLevel)
+    except Exception as measureLightError:
+        logger.debug("Error occured in measureLight: " + str(measureLightError))
+ 
--- sendIM/sendBMEDataToIM_periodically.py
+++ sendIM/sendBMEDataToIM_periodically.py
@@ -6,7 +6,6 @@ import time
 import datetime # datetimeモジュールのインポート
 import locale   # import文はどこに書いてもOK(可読性などの為、慣例でコードの始めの方)
 import os
-import smbus
 import commands
 import sys
 import ConfigParser
@@ -14,6 +13,7 @@ import ambient #ambientにデータを送込むライブラリ
 
 from bme280 import bmeRead
 from retry import retry
+from readBH1750 import measureLight
 
 configfile = ConfigParser.SafeConfigParser() #sftpサーバーへの接続準備
 configfile.read("/home/pi/Documents/mochimugi/config.conf")#絶対パスを使った
@@ -52,85 +52,6 @@ logger.addHandler(fileHandler)
 v0=0.0
 v1=0.0
 
-bus_number = 1
-#bus = SMBus(bus_number) #元はこうなっていた。
-bus = smbus.SMBus(bus_number)
-
-
-class BH1750():
-    """ Implement BH1750 communication. """
-    # Define some constants from the datasheet
-    POWER_DOWN = 0x00 # No active state
-    POWER_ON = 0x01 # Power on
-    RESET = 0x07 # Reset data register value
-
-    # Start measurement at 0.5lx resolution. Time typically 120ms
-    # Device is automatically set to Power Down after measurement.
-    ONE_TIME_HIGH_RES_MODE_2 = 0x21
-
-    def __init__(self, bus, addr=0x23):
-        self.bus = bus
-        self.addr = addr
-        self.power_down()
-        self.set_sensitivity()
-
-    def _set_mode(self, mode):
-        self.mode = mode
-        self.bus.write_byte(self.addr, self.mode)
-
-    def power_down(self):
-        self._set_mode(self.POWER_DOWN)
-
-    def power_on(self):
-        self._set_mode(self.POWER_ON)
-
-    def reset(self):
-        self.power_on() #It has to be powered on before resetting
-        self._set_mode(self.RESET)
-
-    def oneshot_high_res2(self):
-        self._set_mode(self.ONE_TIME_HIGH_RES_MODE_2)
-
-    def set_sensitivity(self, sensitivity=150):
-        """ Set the sensor sensitivity.
-            Valid values are 31 (lowest) to 254 (highest), default is 150.
-        """
-        self.mtreg = sensitivity
-        self.power_on()
-        self._set_mode(0x40 | (self.mtreg >> 5))
-        self._set_mode(0x60 | (self.mtreg & 0x1f))
-        self.power_down()
-
-    def get_result(self):
-        """ Return current measurement result in lx. """
-        data = self.bus.read_word_data(self.addr, self.mode)
-        count = data >> 8 | (data&0xff)<<8
-        mode2coeff = 2 if (self.mode & 0x03) == 0x01 else 1
-        ratio = 1/(1.2 * (self.mtreg/69.0) * mode2coeff)
-        return ratio*count
-
-#    def wait_for_result(self, additional=0):#what is this mean? Setting 0 to parameter.
-    def wait_for_result(self, additional):#what is this mean? Setting 0 to parameter.
-        basetime = 0.018 if (self.mode & 0x03) == 0x03 else 0.128
-        time.sleep(basetime * (self.mtreg/69.0) + additional)
-
-#    def do_measurement(self, mode, additional_delay=0):
-#パラメータを0にリセットしている。これではadditional_delay の指定が有効にならないのでは？
-    def do_measurement(self, mode, additional_delay):
-        """
-        Perform complete measurement using command
-        specified by parameter mode with additional
-        delay specified in parameter additional_delay.
-        Return output value in Lx.
-        """
-        self.reset()
-        self._set_mode(mode)
-        self.wait_for_result(additional=additional_delay)
-        return self.get_result()
-
-    def measure_high_res2(self, additional_delay=1):#was 0. But didn't work.
-        return self.do_measurement(self.ONE_TIME_HIGH_RES_MODE_2, additional_delay)
-
 
 def captureSensorData(i2c_address):
     #センサーからデータ収集するプログラムを実装
@@ -142,25 +63,6 @@ def captureSensorData(i2c_address):
         sys.exit(False)
 
     return temperature, pressure, humid
-
-def measureLight():
-    for lightSense in range(1, 20):
-        try:
-            logger.info("Sensing light level...")
-            #bus = smbus.SMBus(0) # Rev 1 Pi uses 0
-            bus = smbus.SMBus(1)  # Rev 2 Pi uses 1
-            sensor = BH1750(bus)
-            logger.info("Light Sensitivity: {:d}".format(sensor.mtreg))
-            lightLevel = sensor.measure_high_res2()
-            logger.info("Light Level: " + str(lightLevel))
-            return lightLevel
-            break
-        except Exception as error_inMeasureLight:
-            logger.debug("Error during sensing light data: " + str(error_inMeasureLight) + " / " + str(lightSense) + " times trial")
-            time.sleep(2)
-    else:
-        logger.debug("Error during sensing light data after : " + str(lightSense) +" times trial")
-
 
 def sendDataToAmbient():
     ambi = ambient.Ambient(999, ambiKey) # チャネルID、ライトキー
