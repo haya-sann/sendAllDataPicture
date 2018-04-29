ce3f5cb48be9491dda4bad5687ec4a92bc50a2c4
Nobuo Hayashi <haya.biz@gmail.com>
2018-04-30 01:54:38 +0900

    from __init__

--- sendIM/bme280.py
+++ sendIM/bme280.py
@@ -15,7 +15,7 @@ import sys
 
 #from logging import getLogger
 
-from <modname> import get_module_logger
+from __init__ import get_module_logger
 logger = get_module_logger(__name__)
 
 logger.propagate = True
