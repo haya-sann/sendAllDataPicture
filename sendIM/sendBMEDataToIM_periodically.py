34c686a7947196929bac689b2fa1741174d250b3
Nobuo Hayashi <haya.biz@gmail.com>
2018-05-04 18:53:59 +0900

    json.dumps(urlValue)

--- sendIM/sendBMEDataToIM_periodically.py
+++ sendIM/sendBMEDataToIM_periodically.py
@@ -83,7 +83,7 @@ def sendDataToIM():
         if value is not None:
             urlValue += "'" + value_label + ":" + str(value) + ","
     urlValue += "}"
-    params_IM = urllib.urlencode(urlValue)
+    params_IM = urllib.urlencode(json.dumps(urlValue))
 
 #    params_IM = urllib.urlencode({'c': str(imKey), 'date': str(d), 'cpu_temp': cpu_temp, 'temp': temp, 'pressure': pressure/100, 'humid': humid, 'lux' : lightLevel, 'outer_temp': nonesafe_loads(outer_temp), 'outer_pressure': nonesafe_loads(outer_pressure), 'outer_humid': nonesafe_loads(outer_humid), 'log':mochimugiLog, 'deploy' : "sandBox" })    
 
