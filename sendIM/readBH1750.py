78fc18c6537ae1809e3ca7713204e293a6dd891e
Nobuo Hayashi <haya.biz@gmail.com>
2018-05-06 00:26:45 +0900

    repeat sensing light in 5 times

--- sendIM/readBH1750.py
+++ sendIM/readBH1750.py
@@ -91,7 +91,7 @@ class BH1750():
 
 
 def measureLight():
-    for lightSense in range(1, 20):
+    for lightSense in range(1, 5):
         try:
             #bus = smbus.SMBus(0) # Rev 1 Pi uses 0
             bus = smbus.SMBus(1)  # Rev 2 Pi uses 1
