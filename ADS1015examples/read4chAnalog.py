cdfb1306fd72b2a794fda38d82a3cc984f88829d
Nobuo Hayashi <haya.biz@gmail.com>
2018-05-12 02:40:51 +0900

    def read4ch

--- ADS1015examples/read4chAnalog.py
+++ ADS1015examples/read4chAnalog.py
@@ -29,14 +29,15 @@ GAIN = 1
 
 # Read all the ADC channel values in a list.
 values = [0.0]*4
-for i in range(4):
-    # Read the specified ADC channel using the previously set gain value.
-    values[i] = adc.read_adc(i, gain=GAIN)*5.88/267
-    # Note you can also pass in an optional data_rate parameter that controls
-    # the ADC conversion time (in samples/second). Each chip has a different
-    # set of allowed data rate values, see datasheet Table 9 config register
-    # DR bit values.
-    #values[i] = adc.read_adc(i, gain=GAIN, data_rate=128)
-    # Each value will be a 12 or 16 bit signed integer value depending on the
-    # ADC (ADS1015 = 12-bit, ADS1115 = 16-bit).
+def read4ch(values):
+    for i in range(4):
+        # Read the specified ADC channel using the previously set gain value.
+        values[i] = adc.read_adc(i, gain=GAIN)*5.88/267
+        # Note you can also pass in an optional data_rate parameter that controls
+        # the ADC conversion time (in samples/second). Each chip has a different
+        # set of allowed data rate values, see datasheet Table 9 config register
+        # DR bit values.
+        #values[i] = adc.read_adc(i, gain=GAIN, data_rate=128)
+        # Each value will be a 12 or 16 bit signed integer value depending on the
+        # ADC (ADS1015 = 12-bit, ADS1115 = 16-bit).
 
--- ADS1015examples/testReadPlobe.py
+++ ADS1015examples/testReadPlobe.py
@@ -1,5 +1,5 @@
-import read4chAnalog
+import from read4chAnalog read4ch
 
 values = [0.0]*4
-read4chAnalog
+read4ch
 print('| {0:>6,.2f} | {1:>6,.2f} | {2:>6,.2f} | {3:>6,.2f} |'.format(*values))
