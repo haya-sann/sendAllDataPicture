a93c04824e36ffd493d9fab80232a0b0211cba9a
Nobuo Hayashi <haya.biz@gmail.com>
2018-06-02 00:19:23 +0900

    def capture_send():

--- sendIM/sendBMEDataToIM_periodically.py
+++ sendIM/sendBMEDataToIM_periodically.py
@@ -29,7 +29,6 @@ from sendMail import send, create_message
 from w1_DS18B20 import read_soil_temp
 from ftps import sendLog_ftps
 import picamera
-from cameraTimedCapture import capture_send
 
 try:
     DEPLOY_SWITCH = os.environ['DEPLOY']
@@ -133,6 +132,39 @@ def sendDataToIM():
     logger.info("Server respond:" + str(response.status) + str(response.reason))
     data = response.read()
     conn.close()
+def capture_send():
+    logger.info('Waiting for shooting time')
+    while True:
+        now = datetime.datetime.now()
+        if now.minute % everyMinutes <= 7: #指定毎分時、過ぎた場合は7分以内なら正常撮影
+            logger.info('指定時間になりました')
+            captureFile_name = now.strftime('%Y%m%d%H%M') + '.jpg'
+            break
+        elif everyMinutes - (now.minute % everyMinutes) > 7:#7分より多く待つなら取りあえず撮影して終わる
+            logger.info('指定時間まで7分以上ありますので、テスト撮影して指定時刻5分前に再起動します')
+            captureFile_name = '電源投入時テスト_' + now.strftime('%Y%m%d%H%M') + '.jpg'
+            break
+    logger.info('写真の保存ファイル名；' + captureFile_name)
+    picamera.start_preview() #あれ？　これ入れてなかったよ。これがないと露出調整がうまくいかないんじゃ？　2017/06/14
+    time.sleep(2) #これも入れ忘れてた　2017/06/14　12:59
+#    picamera.brightness = 55 #標準の50よりほんの少し明るめに
+    picamera.contrast = pictureContrast 
+#標準の50よりほんの少しコントラストを強めに
+    logger.info("brightness:"+str(picamera.brightness)+" ,contrast:"+str(picamera.contrast))
+    # picamera.annotate_background = picamera.Color('black')
+    picamera.annotate_text = now.strftime('%Y-%m-%d %H:%M:%S') + " , Contrast : " + str(picamera.contrast)
+    picamera.rotation = 180
+    picamera.capture(captureFile_name)
+    
+    try:
+        send_ftps(captureFile_name)
+        logger.info("File is sended with no error. Delete " + captureFile_name + " on Ras Pi")
+        os.remove(captureFile_name)
+        return captureFile_name
+
+    except:
+        logger.info("Failed file transfer in send_ftps。そのまま何もしない")
+
 
 # today()メソッドで現在日付・時刻のdatetime型データの変数を取得
 d = datetime.datetime.today()
@@ -189,7 +221,7 @@ try:
     hour = now.hour
 
     if hour >= hourToBegin -1 and hour < hourToStop: #動作は止める時刻になる前まで
-        localFile_name = capture_send(everyMinutes) #写真撮影し、結果をサーバーに送信、送信ファイル名を受け取る
+        localFile_name = capture_send() #写真撮影し、結果をサーバーに送信、送信ファイル名を受け取る
 
     now = datetime.datetime.now()
     hour = now.hour
