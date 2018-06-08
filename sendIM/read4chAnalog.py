e8b5f966a071e9d8bc02b226d086476c6df8e0f6
Nobuo Hayashi <haya.biz@gmail.com>
2018-06-08 14:50:14 +0900

    error try in 4ch analog read

--- sendIM/read4chAnalog.py
+++ sendIM/read4chAnalog.py
@@ -32,7 +32,11 @@ values = [0.0]*4
 def read4ch(values):
     for i in range(4):
         # Read the specified ADC channel using the previously set gain value.
-        values[i] = adc.read_adc(i, gain=GAIN)*5.88/267
+        try:
+            values[i] = adc.read_adc(i, gain=GAIN)*5.88/267
+        except Exception as error_in_Read4ChData:
+            logger.debug("Error during sensing 4ch analog data: " + str(error_in_Read4ChData) + " / " + str(i) + " times trial")
+
         # Note you can also pass in an optional data_rate parameter that controls
         # the ADC conversion time (in samples/second). Each chip has a different
         # set of allowed data rate values, see datasheet Table 9 config register
