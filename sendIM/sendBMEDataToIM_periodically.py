af9220edcb1c12f32a76f59fa61010e786a5c474
Nobuo Hayashi <haya.biz@gmail.com>
2018-06-14 00:36:59 +0900

    hourToBegin = 1

--- sendIM/sendBMEDataToIM_periodically.py
+++ sendIM/sendBMEDataToIM_periodically.py
@@ -87,7 +87,7 @@ pictureContrast = 10
 # pictureBrightness =55
 # pictureContrast = 10
 
-hourToBegin = 0 #カメラを動作開始させる時刻
+hourToBegin = 1 #カメラを動作開始させる時刻
 hourToStop = 24 #カメラを完全休止させる時刻
 everyMinutes = 60 #何分おきに撮影するのかをセット。5~60の値をセット
 
