6249892d1fae159ce1d2f355c29c7755cc77a476
Nobuo Hayashi <haya.biz@gmail.com>
2018-06-13 22:56:06 +0900

    pictureContrast = 20

--- sendIM/sendBMEDataToIM_periodically.py
+++ sendIM/sendBMEDataToIM_periodically.py
@@ -83,7 +83,7 @@ logger.info("資料の保存先は：" + put_directory)
 #カメラ撮影準備
 localFile_name = ""
 pictureBrightness =60
-pictureContrast = 30
+pictureContrast = 20
 # pictureBrightness =55
 # pictureContrast = 10
 
