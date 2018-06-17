55f75cb77036bec9a9a7c1c0e325a6d0c90d2622
Nobuo Hayashi <haya.biz@gmail.com>
2018-06-17 14:04:39 +0900

    import codecs

--- sendIM/ftps.py
+++ sendIM/ftps.py
@@ -43,6 +43,7 @@ def sendLog_ftps(file_name, put_directory):
         _file.close()
         _ftps.quit()
         #log送信正常終了なので、中身をクリアする
+        import codecs
         with open('/var/log/' + file_name, 'w', 'utf-8') as f:
             f.write("Upload finished with no error. Log cleared at: " + _timeStamp.strftime('%Y%m%d%H%M') + "\n")
             f.close()
