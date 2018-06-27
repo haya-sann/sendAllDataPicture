742b8572be5df7cdec0b10e75abfec3ed2c713f4
Nobuo Hayashi <haya.biz@gmail.com>
2018-06-27 18:55:43 +0900

    send boot.log

--- sendIM/sendBMEDataToIM_periodically.py
+++ sendIM/sendBMEDataToIM_periodically.py
@@ -343,6 +343,13 @@ except Exception as e:
         logger.debug("sendLog_ftps error. :" + str(e))
         f.close()
 
+file_name = "boot.log"
+try:
+    _timeStamp = sendLog_ftps(file_name, put_directory)
+
+except Exception as e:
+    logger.debug("send boot.log ftps error . :" + str(e))
+
 
 #            f.write(unicode ((u'アップロード終了 with no error. Log cleared at: ' + _timeStamp.strftime(u'%Y%m%d%H%M') + u'\n').encode('utf_8','ignore'),'utf_8'))
 #            f.close() #with openの場合、これは不要らしい。
