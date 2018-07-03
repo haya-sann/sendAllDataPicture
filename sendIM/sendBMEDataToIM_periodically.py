f284f4cb7aba63acabf009e1b3ba9759a08932df
Nobuo Hayashi <haya.biz@gmail.com>
2018-07-04 01:48:24 +0900

    every 15 minutes for debug

--- sendIM/sendBMEDataToIM_periodically.py
+++ sendIM/sendBMEDataToIM_periodically.py
@@ -91,7 +91,7 @@ pictureSharpness = 20
 
 hourToBegin = 1 #カメラを動作開始させる時刻
 hourToStop = 24 #カメラを完全休止させる時刻
-everyMinutes = 60 #何分おきに撮影するのかをセット。5~60の値をセット
+everyMinutes = 15 #何分おきに撮影するのかをセット。5~60の値をセット
 
 
 v0=v1=soil1=soil2=soil_temp=0.0
