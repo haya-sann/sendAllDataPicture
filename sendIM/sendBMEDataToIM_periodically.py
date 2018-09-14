294b8b3f7476cf88e4e7f6e6f647772529f4ecb3
Nobuo Hayashi <haya.biz@gmail.com>
2018-09-14 15:34:54 +0900

    いよいよmasterに統合-本番移行

--- sendIM/sendBMEDataToIM_periodically.py
+++ sendIM/sendBMEDataToIM_periodically.py
@@ -91,7 +91,7 @@ pictureSharpness = 20
 
 hourToBegin = 5 #カメラを動作開始させる時刻
 hourToStop = 19 #カメラを完全休止させる時刻
-everyMinutes = 10 #何分おきに撮影するのかをセット。5~60の値をセット
+everyMinutes = 60 #何分おきに撮影するのかをセット。5~60の値をセット
 
 
 v0=v1=soil1=soil2=soil_temp=0.0
