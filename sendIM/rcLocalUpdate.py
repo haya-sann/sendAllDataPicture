3f0971e0b9df8c84f9cccb2281eca8580ed7ee57
Nobuo Hayashi <haya.biz@gmail.com>
2018-11-01 18:50:07 +0900

    should be replaced whenever

--- sendIM/rcLocalUpdate.py
+++ sendIM/rcLocalUpdate.py
@@ -5,7 +5,7 @@ import os
 
 def updateRCLocal():
 
-    os.system("sudo cp -nv /home/pi/Documents/field_location/sendAllDataPicture/rcLocalUpdate.sh /etc/rc.local")
+    os.system("sudo cp /home/pi/Documents/field_location/sendAllDataPicture/rcLocalUpdate.sh /etc/rc.local")
 
 updateRCLocal()
 
