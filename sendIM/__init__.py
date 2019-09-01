017a03a68c9575e41c7a3ee72ed0c2198034089c
Nobuo Hayashi <haya.biz@gmail.com>
2019-09-02 00:15:14 +0900

    revert __init___

--- sendIM/__init__.py
+++ sendIM/__init__.py
@@ -7,12 +7,7 @@ def get_module_logger(modname):
     logger.addHandler(handler)
     logger.setLevel(logging.DEBUG)
 
-    # root権限に昇格
-    if os.geteuid():
-        args = [sys.executable] + sys.argv
-        os.execlp(‘sudo’, ‘sudo’, *args)
     fileHandler = logging.FileHandler('/var/log/field_location.log', mode='a', encoding=None, delay=0)
-
 
     fileHandler.setFormatter(formatter)
     fileHandler.setLevel(logging.DEBUG)
