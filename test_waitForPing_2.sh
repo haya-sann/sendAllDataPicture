0fc3b29225598d711c8ec1ddcf7e04539644032c
Nobuo Hayashi <haya.biz@gmail.com>
2019-09-02 21:32:59 +0900

    fix ping command

--- test_waitForPing_2.sh
+++ test_waitForPing_2.sh
@@ -3,7 +3,7 @@
 function waitForPing1() {
     # Wait for Network to be available.
 for i in {1..3};
-    do ping -c1 ping -c1 ciao-kawagoesatoyama.ssl-lolipop.jp &> /dev/null && (echo Server is available) && break; 
+    do ping -c1 ciao-kawagoesatoyama.ssl-lolipop.jp &> /dev/null && (echo Server is available) && break; 
     echo -n .
     done
 [ $i = 3 ] && echo Can not reach Server && return 1
