7dc96504110ff76c131b937ea06ff0f3abde72a9
Nobuo Hayashi <haya.biz@gmail.com>
2019-09-07 00:33:58 +0900

    exit 0

--- sendIM/gitPull.sh
+++ sendIM/gitPull.sh
@@ -5,3 +5,5 @@ cd /home/pi/Documents/field_location/sendAllDataPicture
 git checkout ${gitBranch} | tee -a ${LOGFILE} #|| log ("Error occured in git. Update failed")
 git status | tee -a ${LOGFILE} # || log ("Error occured in git. Update failed")
 git pull | tee -a ${LOGFILE} # || log ("Error occured in git. Update failed")
+
+exit 0
