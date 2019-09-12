5bcc89e2b079f3b18cbe2075c603d326444cd90e
Nobuo Hayashi <haya.biz@gmail.com>
2019-09-12 23:48:03 +0900

    loger.info(_timeStamp)

--- sendIM/sendBMEDataToIM_periodically.py
+++ sendIM/sendBMEDataToIM_periodically.py
@@ -173,6 +173,7 @@ if os.path.isfile(src):
             f.write ('アップロード終了 with no error. Log cleared at: ' + _timeStamp.strftime('%Y%m%d%H%M').encode('utf_8') + '\n'.encode('utf_8'))
         f.close()
     except Exception as e:
+            loger.info(_timeStamp)
             logger.debug("sendLog_ftps error. :" + str(e))
 
 logger.info("公開先は：" + DEPLOY_SWITCH)
