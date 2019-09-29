41a2c06f4a6388f4bdab1a4343a0764b40c62cf5
Nobuo Hayashi <haya@Air-3.local>
2019-09-29 09:49:37 +0900

    typo

--- sendIM/checkNetwork.py
+++ sendIM/checkNetwork.py
@@ -1,8 +1,8 @@
 import subprocess
 try:
-        if subprecess.call ("bash test_waitForPing.sh") !=0:
+        if subprocess.call ("bash test_waitForPing.sh") !=0:
                 raise Exception('Can not reach the server')
         print("Server can be reached")
 except Exception as e:
         print ('Error. ' + str(e))
-        subprecess.call('sudo reboot')
+        subprocess.call('sudo reboot')
