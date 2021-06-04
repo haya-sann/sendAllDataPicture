bf1ef49455db02f50728851f9dfe3f2317fbe849
Nobuo Hayashi <haya.biz@gmail.com>
2021-06-04 23:06:34 +0900

    class Color:

--- sendIM/sr04_Bluetin_sendAmbient.py
+++ sendIM/sr04_Bluetin_sendAmbient.py
@@ -11,6 +11,22 @@ https://github.com/MarkAHeywood/Bluetin_Python_Echo/blob/master/examples/echo_si
 """ 
 # 実行ユーザーが誰なのか、調べるコードを追加
 # 
+class Color:
+    BLACK     = '\033[30m'
+    RED       = '\033[31m'
+    GREEN     = '\033[32m'
+    YELLOW    = '\033[33m'
+    BLUE      = '\033[34m'
+    PURPLE    = '\033[35m'
+    CYAN      = '\033[36m'
+    WHITE     = '\033[37m'
+    END       = '\033[0m'
+    BOLD      = '\038[1m'
+    UNDERLINE = '\033[4m'
+    INVISIBLE = '\033[08m'
+    REVERCE   = '\033[07m'
+
+
 import sys, os
 #os.seteuid(1000)
 os.system('ps -u')
