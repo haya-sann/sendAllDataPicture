b78c9ebc8599b54c713cbe2b10a4acb58c2d5f5c
Nobuo Hayashi <haya.biz@gmail.com>
2021-06-08 15:27:33 +0900

    refine start.sh

--- sendIM/start.sh
+++ sendIM/start.sh
@@ -1,2 +1,3 @@
-#データ送信プログラム本体を起動する（2021/06/04、12:10）
-python /home/pi/Documents/field_location/sendAllDataPicture/sendIM/sendBMEDataToIM_periodically.py
+#データ送信プログラム本体を起動する（2021/06/08 15:24）
+python ~/Documents/tanboCamera/sendAllDataPicture/sendIM/sendBMEDataToIM_periodically.py
+#python /home/pi/Documents/field_location/sendAllDataPicture/sendIM/sendBMEDataToIM_periodically.py
