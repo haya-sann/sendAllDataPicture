189362b6195c3aaa8ab8754cb0fc8dd841c51bc1
Nobuo Hayashi <haya.biz@gmail.com>
2021-07-01 16:46:03 +0900

    averageDepth with temperature

--- sendIM/sendBMEDataToIM_periodically.py
+++ sendIM/sendBMEDataToIM_periodically.py
@@ -373,7 +373,7 @@ try:
 except Exception as error_soilTemperature:
     logger.debug("Error occured in measure soil temperature" + str(error_soilTemperature))
 
-averageDepth = sr04_read() # call depth measurment
+averageDepth = sr04_read(outer_temp) # call depth measurment
 #Send atmosphere data to AmbiData
 try:
     sendDataToAmbient()
