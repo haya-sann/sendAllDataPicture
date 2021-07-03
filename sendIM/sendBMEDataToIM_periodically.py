ab4792bcd1ab16d76c0bd85310e3386333282068
Nobuo Hayashi <haya.biz@gmail.com>
2021-07-03 13:39:46 +0900

    pictureBanner = "Test Shooting。1280"

--- sendIM/sendBMEDataToIM_periodically.py
+++ sendIM/sendBMEDataToIM_periodically.py
@@ -85,7 +85,7 @@ pictureContrast = 10
 # pictureBrightness =55
 # pictureContrast = 10
 pictureSharpness = 20
-pictureBanner = "Test Shooting。1920"
+pictureBanner = "Test Shooting。1280"
 
 v0=v1=soil1=soil2=soil_temp=0.0
 temperature = 28.55 #川越の8月の平均温度。取得できないときはこれを使う
