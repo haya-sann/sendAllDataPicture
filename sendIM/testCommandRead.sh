54a8a26d5ad3842419ecb8affd57010fc3076429
Nobuo Hayashi <haya.biz@gmail.com>
2021-07-19 12:39:08 +0900

    programmerSwitch = "on"

--- sendIM/testCommandRead.sh
+++ sendIM/testCommandRead.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-switch=`grep -o "programmerSwitch = \"on\"" debugControl.py` #シングルクオーテーションじゃなくてバッククオート
+grep -o "programmerSwitch = \"on\"" debugControl.py #シングルクオーテーションじゃなくてバッククオート
 
-echo $switch
-switch=`echo $switch | sed -e s/^programmerSwitch\ =\ \"on\"/on/`
-echo $switch
 
+if [ $? -eq 0 ] ; then #直前のコマンド実行結果を$?が保持している。これを調べれば検査したい文脈があるかどうか分かる
+#grepコマンドを実行した後、すぐに判定しないと$?が正しく動作しない
+  echo program switch is ON
+else echo program switch is OFF
+fi
