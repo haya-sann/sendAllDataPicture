59db4efa5bc24de3c4fda1206be8e21bdac236ff
Nobuo Hayashi <haya.biz@gmail.com>
2021-08-14 21:28:43 +0900

    Revert "Delete test_waitForPing.sh"
    This reverts commit c25785c4f8837873a8bc53c7bf3b57bed94b697c.

---
+++ sendIM/test_waitForPing.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#gitの再利用を始めた。2021/06/04、12時03分
+function waitForPing1 (){
+    # Wait for Network to be available.
+for i in {1..3};
+    do ping -c1 ciao-kawagoesatoyama.ssl-lolipop.jp &> /dev/null && break;
+    echo -n .
+    done
+[ $i = 3 ] && exit 1
+return 0
+}
+
+waitForPing1
